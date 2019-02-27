71d7061ec4d7cd89eeb7fc3be8003f495dd87a66
Sam Gluss <sam@ople.ai>
2019-02-27 13:43:56 -0800

    redis hook is working

--- src/CZMQ-ZWSSock/main.c
+++ src/CZMQ-ZWSSock/main.c
@@ -1,14 +1,60 @@
 #include <czmq.h>
 
 #include "zwssock.h"
+#include "hiredis/hiredis.h"
 
 #define ZMQ_STATIC
 
 static char *listen_on = "tcp://127.0.0.1:8000";
 
+redisContext* gimmeRedis() {
+	redisContext *c;
+    redisReply *reply;
+
+    printf("setting host\n");
+    const char *hostname = "127.0.0.1";
+
+    int port = 6379;
+
+    struct timeval timeout = { 1, 500000 }; // 1.5 seconds
+
+	printf("establishing connection to %s\n", hostname);
+	c = redisConnectWithTimeout(hostname, port, timeout);
+
+    if (c == NULL || c->err) {
+        if (c) {
+            printf("Connection error: %s\n", c->errstr);
+            redisFree(c);
+        } else {
+            printf("Connection error: can't allocate redis context\n");
+        }
+        exit(1);
+    }
+
+    /* PING server */
+    reply = redisCommand(c,"PING");
+    printf("PING: %s\n", reply->str);
+    freeReplyObject(reply);
+
+    if (strcmp(reply->str, "PONG")) {
+    	printf("redis connection established successfully!\n\n");
+		return c;
+	} else {
+		printf("failed to connect to redis!\n\n");
+		return NULL;
+    }
+}
+
 int main(int argc, char **argv)
 {
+	//redisContext* redis = gimmeRedis();
+
 	zwssock_t *sock;
+
+	zsock_t *dealer = zsock_new (ZMQ_DEALER);
+	int dealer_port = zsock_connect (dealer, "tcp://127.0.0.1:5570");
+
+	printf("dealer is connected to port %d\n", dealer_port);
 
 	char *l =  argc > 1 ? argv[1] : listen_on;
 
